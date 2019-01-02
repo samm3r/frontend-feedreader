6b41f9adf8eb8302625462109c307c150626e5bd
Sammer Soares <sammer.davio@gmail.com>
2019-01-01 23:54:22 -0200

    implemented jasmine tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -21,7 +21,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('variable is defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,30 +31,70 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('name is defined', function() {
+            for (var i in allFeeds){
+                expect(allFeeds[i].name).toBeDefined();
+                expect((allFeeds[i].name).length).not.toBe(0);
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    });
+         it('url is not empty', function() {
+            for (var i in allFeeds){
+                expect(allFeeds[i].url).toBeDefined();
+                expect((allFeeds[i].url).length).not.toBe(0);
+            }
+
+        });
+
+     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+        var body,
+            menuIcon;
+
+        beforeEach(function(){
+            body = document.querySelector('body');
+            menuIcon = document.querySelector('.menu-icon-link');
+        });
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('menu is hidden by default', function() {
+            expect(body.className).toBe('menu-hidden');
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('menu changes visibility onclick', function() {
+            menuIcon.click();
+            expect(body.className).not.toBe('menu-hidden');
+            menuIcon.click();
+            expect(body.className).toBe('menu-hidden');
+         });
+    });
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -62,11 +102,37 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         it('feeds are loaded properly', function(done) {
+            expect($('.feed .entry').length).not.toBe(0);
+            done();
+         })
+         
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        var feedOne,
+            feedTwo;
+
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                feedOne = $('.feed .entry');
+                loadFeed(1, function() {
+                    feedTwo = $('.feed .entry');
+                    done();
+                });
+            });
+        });
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         it('content is changed properly', function(done) {
+            expect(feedOne).not.toEqual(feedTwo);
+            done();
+         })
+
+    });
+
 }());
